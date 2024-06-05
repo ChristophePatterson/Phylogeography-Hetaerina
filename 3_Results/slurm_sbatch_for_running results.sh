03fce66e400d537224f7751b57fb53df8d601fe3
ChristophePatterson <christophepatterson@outlook.com>
2024-06-05 15:30:48 +0100

    Slurm order improvement and running LEA

--- 3_Results/slurm_sbatch_for_running results.sh
+++ 3_Results/slurm_sbatch_for_running results.sh
@@ -7,4 +7,12 @@ sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/SV
 sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/RAxML/9_RAxML_array.sh
 sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/LEA/LEA_PCA.sh
 
+## After running LEA
+sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/G-Phocs/g-phocs-run.sh
+sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/SNAPP/SNAPP_run.sh
 
+## Runnning delimitR
+# Run 1
+sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/delimitR/easySFS_2_delimitR.sh
+# Then run 2 (after checking downprojecting is correct)
+sbatch /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/3_Results/delimitR/easySFS_2_delimitR.sh
