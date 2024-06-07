add0110e142a8bc774d208c657e74f7897336561
ChristophePatterson <christophepatterson@outlook.com>
2024-06-07 17:33:25 +0100

    Convert RAD loci calling code to slurm

--- 2_SNP_calling/8_RAD_loci_calling.sh
+++ 2_SNP_calling/8_RAD_loci_calling.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 #SBATCH -c 1 
-#SBATCH --mem=20G            # memory required, in units of k,M or G, up to 250G.
-#SBATCH --gres=tmp:20G       # $TMPDIR space required on each compute node, up to 400G.
-#SBATCH -t 24:00:00         # time limit in format dd-hh:mm:ss
+#SBATCH --mem=50G            # memory required, in units of k,M or G, up to 250G.
+#SBATCH --gres=tmp:50G       # $TMPDIR space required on each compute node, up to 400G.
+#SBATCH -t 48:00:00         # time limit in format dd-hh:mm:ss
 
 # Specify the tasks to run:
 #SBATCH --array=1-6  # Create 6 tasks, numbers 1 to 6
@@ -51,7 +52,7 @@ cat $library_version/$Library_name/samples_temp.txt | while read line; do
     filename=${filename:0:-11}
     echo $filename
     samtools faidx $genome -r $library_version/$Library_name/${Library_name}_RAD_posistions.txt | \
-    bcftools consensus -s $line -e 'FORMAT/AD<5 & FORMAT/AD>200 & QUAL<50' -a "?" -o $library_version/$Library_name/fasta_files/${filename}.fasta $library_version/$Library_name/$Library_name.all.snps.bcf 
+    bcftools consensus -s $line -e 'FORMAT/AD<5 & FORMAT/AD>200 & QUAL<50' -a "?" --iupac-codes -o $library_version/$Library_name/fasta_files/${filename}.fasta $library_version/$Library_name/$Library_name.all.snps.bcf 
 done 
 
 Rscript /home/tmjj24/scripts/Github/Thesis-Phylogeographic-Hetaerina/2_SNP_calling/8_RAD_loci_calling_phylip.R $Library_name $library_version
